57b6d4f6e2b11d358dc36e48e7825676cb5c516e
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2026-01-03 11:35:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,57 +9,6 @@ let monsterHealth;
 let inventory = ["stick"];
 let objects = ["orb", "key", "skull"]
 
-const locations = [
-    {
-        name: "town square",
-        "button text": ["Go to store", "Go to cave", "Fight dragon"],
-        "button functions": [goStore, goCave, fightDragon],
-        text: "You are in the town square. You see a sign that says \"Store.\""
-    },
-	{
-		name: "store",
-		"button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-		"button functions": [buyHealth, player.buyWeapon, goTown],
-		text: "You enter the store."
-	},
-	{
-		name: "cave",
-		"button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-		"button functions": [fightSlime, fightBeast, goTown],
-		text: "You enter the cave. You see some monsters."
-	},
-	{
-		name: "fight",
-		"button text": ["Attack", "Dodge", "Run"],
-		"button functions": [attack, dodge, goTown],
-		text: "You are fighting a monster."
-	},
-	{
-		name: "kill monster",
-		"button text": ["Go to town square", "Go to town square", "Go to town square"],
-		"button functions": [goTown, goTown, easterEgg],
-		text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
-	},
-	{
-		name: "lose",
-		"button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
-		"button functions": [restart, restart, restart],
-		text: "You die. ☠️"
-	},
-	{
-		name: "win",
-		"button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
-		"button functions": [restart, restart, restart],
-		text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
-    },
-	{
-		name: "easter egg",
-		"button text": ["2", "8", "Go to town square?"],
-		"button functions": [pickTwo, pickEight, goTown],
-		text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
-	}
-]
-
 function buyHealth() {
     if (gold >= 10) {
         gold -= 10;
